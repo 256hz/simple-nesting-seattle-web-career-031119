393e26ea686368a8891c04a2ac84efdf845537b6
Abe D <iaryiou111@gmail.com>
2019-03-05 16:11:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,16 +55,10 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages]
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -84,8 +78,7 @@ def adding_matz
       known_for: "Ruby",
       languages: ["LISP", "C"]
     }
-
-    
+  programmer_hash[:yukihiro_matsumoto]
 end
 
 def changing_alan
@@ -107,6 +100,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
   programmer_hash[:alan_kay][:known_for] = alans_new_info
+  programmer_hash[:alan_kay][:known_for]
      
 end
 
@@ -127,5 +121,5 @@ def adding_to_dennis
         }
      }
     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
-
+    programmer_hash[:dennis_ritchie][:languages]
 end
