a8fdade62b2715054050992ed85a4c8216fae4fe
Abe D <iaryiou111@gmail.com>
2019-03-05 16:14:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -121,5 +121,5 @@ def adding_to_dennis
         }
      }
     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
-    programmer_hash[:dennis_ritchie][:languages]
+    programmer_hash
 end
